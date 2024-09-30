a93132720bc30999e43d9dea4fb85004ad562732
begumdoganay <86783970+begumdoganay@users.noreply.github.com>
2024-09-30 10:34:34 +0300

    Add files via upload

---
+++ Console6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HadiDüldülBirinciHafta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "Begüm";
+            string soyad = "Doğanay";
+            string neden = "Yaşadığım akademik ve kariyer yönlü krizlere son verip yeni alan elde edip o şekilde geleceğimi inşa etmek adına ve 25 yaşımı yeni bir ben elde etmek gayesiyle buradayım.";
+
+            Console.WriteLine("Adım: " + ad);
+            Console.WriteLine("Soyadım: " + soyad);
+            Console.WriteLine("Patika Bootcamp'te olma nedenim: " + neden);
+
+            Console.ReadLine();
+        }
+    }
+}
